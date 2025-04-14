8d864ea7697246bbeb6b997adc28fdd31c3ea376
Samarth <119740787+Samartha0007@users.noreply.github.com>
2025-04-14 10:36:09 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex', justifyContent: 'center',
+      alignItems: 'center', height: '100vh', fontSize: '2rem'
+    }}>
+      Add a name in the URL like <code>/samarth</code>
+    </div>
+  );
+}
